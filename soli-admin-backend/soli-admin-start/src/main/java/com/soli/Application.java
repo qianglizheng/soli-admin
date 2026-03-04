29eddae255d08ab2bad75b1739e4d91b463d15e9
lizhengqiang <1648785@qq.com>
2026-03-04 23:04:47 +0800

    feat:搭建后端架构，引入部分依赖

---
+++ soli-admin-backend/soli-admin-start/src/main/java/com/soli/Application.java
@@ -0,0 +1,17 @@
+package com.soli;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @author lizhengqiang
+* @since 2026-03-04 22:08
+*/
+@SpringBootApplication(scanBasePackages = "com.soli")
+@MapperScan(basePackages = "com.soli")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
